84314494fd18bd3de63840fca1a68b02e7b78187
HY <tlshy002@gmail.com>
2024-01-31 15:23:11 +0900

    map()함수로 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function Food({ name, picture }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>I like {name}</h3>
+      <img src={picture} alt={name}/>
+    </div> 
+  );
+}
+// const foodILike = [ {컴포넌트}, {컴포넌트}, ...]
+const foodILike = [
+  {
+    id: 1,
+    name: 'cat',
+    image: 'https://src.hidoc.co.kr/image/lib/2022/5/12/1652337370806_0.jpg',
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: 'dog',
+    image: 'https://src.hidoc.co.kr/image/lib/2022/5/12/1652337370806_0.jpg',
+    rating: 4.8,
+  },
+  {
+    id: 3,
+    name: 'hamster',
+    image: '',
+    rating: 5,
+  },
+  {
+    id: 4,
+    name: 'rabbit',
+    image:'',
+    rating: 3.5,
+  },
+  {
+    id: 5,
+    name: 'panda',
+    image: '',
+    rating: 4.5,
+  }
+];
+
+// App컴포넌트를 정의함
+function App() {
+  return ( // html반환
+    <div>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image}/>
+      ))}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
+// 리액트는 ReactDOM
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'; // App컴포넌트 import
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+// 리액트는 최종적으로 단 하나의 컴포넌트만을 가짐
+// 그래서 새로 만든 컴포넌트를 App컴포넌트 안에 넣어야 함
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
