47bfa94c444d0ee69f3d0a50bf7fddaed60bbe6e
HY <tlshy002@gmail.com>
2024-02-02 16:51:06 +0900

    05-2 숫자 증감기능

--- src/App.js
+++ src/App.js
@@ -6,21 +6,26 @@ class App extends React.Component {
     count:0,
   }
   add = () => {
-    console.log('add');
+    this.setState(current => ( 
+      {count: current.count + 1,}
+      ));
   };
   minus = () => {
-    console.log('minus');
+    this.setState(current => (
+      {count: current.count -1,}
+    ));
   };
-  
+
   render() {
     return (
       <div>
         <h1>The number is : {this.state.count}</h1>
-        <button>Add</button>
-        <button>Minus</button>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
       </div>
     
 
     );
   }
 }
+export default App;
