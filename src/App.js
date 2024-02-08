6a13dea94b8d3cf7e42f10784582d37ebae1bcc0
HY <tlshy002@gmail.com>
2024-02-08 15:44:31 +0900

    05-3 클래스형컴포넌트 생명주기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 
 // 클래스형 컴포넌트를 사용하는 이유 => state를 사용하기 위해
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('hello');
+  }
+
   state = {
     count:0,
   }
@@ -15,8 +20,18 @@ class App extends React.Component {
       {count: current.count -1,}
     ));
   };
+  componentDidMount() {
+    console.log("conmponent rendered");
+  }
+  componentDidUpdate() {
+    console.log('I just update!');
+  }
+  componentWillUnmount() {
+    console.log('Goodbye, cruel world');
+  }
 
   render() {
+    console.log("I'm rendering");
     return (
       <div>
         <h1>The number is : {this.state.count}</h1>
